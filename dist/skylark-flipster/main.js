46463b2bcbe6df292e0608cdd8661be65c6aea51
hudaokeji <github@hudaokeji.com>
2020-03-29 17:16:49 +0900

    init commit

---
+++ dist/skylark-flipster/main.js
@@ -0,0 +1,9 @@
+/**
+ * skylark-flipster - A version of flipster.js that ported to running on skylarkjs.
+ * @author Hudaokeji, Inc.
+ * @version v0.9.0
+ * @link https://github.com/skylark-integration/skylark-flipster/
+ * @license MIT
+ */
+define(["./flipster"],function(e){return e});
+//# sourceMappingURL=sourcemaps/main.js.map
---
+++ src/main.js
@@ -0,0 +1,5 @@
+define([
+	"./flipster"
+],function(flipster) {
+	return flipster;
+});
